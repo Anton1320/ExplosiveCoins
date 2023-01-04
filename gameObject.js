3e9916cc71c65c3dc328f25ad290faffda82d80c
Anton <56134229+Anton1320@users.noreply.github.com>
2023-01-04 21:22:31 +0300

    Create gameObject.js

---
+++ gameObject.js
@@ -0,0 +1,9 @@
+class gameObject
+{
+    constructor(pos, color, className)
+    {
+        this.pos = {x:pos[0], y:pos[1]};
+        this.className = className;
+        this.color = color;
+    }    
+}
