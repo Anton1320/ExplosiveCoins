65ae7340995bd31c85d795badb0f2de690620afa
Anton <56134229+Anton1320@users.noreply.github.com>
2023-01-05 12:54:12 +0300

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -1,9 +1,75 @@
-var leaderboard = document.getElementById("leaderboard");
-leaderboard.width = sc.clientWidth/3
-leaderboard.height = sc.clientHeight/2;
-var lbctx = leaderboard.getContext("2d");
+class Leaderboard {
+    constructor() {
+        this.background = new Item([screenSize.x, 0], [sc.offsetWidth-screenSize.x, screenSize.y], "lightblue", "leaderboard");
+        this.playerInfo = {rank: 0, publicName: "currently unavailable"};
+        this.topPlayers = [];
+        for (let i = 0; i < 10; ++i) {
+            this.topPlayers.push({rank: 0, publicName: "currently unavailable"});
+        }
+        this.topPlayersTexts = [];
+        for (let i = 0; i < 10; ++i) {
+            this.topPlayersTexts.push(new textObject(
+                [this.background.pos.x+5, this.background.pos.y+20+40*i],
+                this.topPlayers[i].rank + " " + this.topPlayers[i].publicName,
+                "black",
+                ""));
+        }
+        this.playerRateText = new textObject([this.background.pos.x+5, this.background.pos.y+5+40*11],
+            this.playerInfo.rank + " " + this.playerInfo.publicName,
+            "black",
+            "");
 
-var a = new Item([100, 100], [100, 100], "black", "smth");
+    }
 
-a.draw(lbctx);
-console.log("lb is ok!");
+    updateText() {
+        for (let i = 0; i < this.topPlayers.length; ++i) {
+            this.topPlayersTexts[i].text = this.topPlayers[i].rank + " " + this.topPlayers[i].publicName;
+        }
+        this.playerRateText.text = this.playerInfo.rank + " " + this.playerInfo.publicName;
+    }
+
+    draw() {
+        this.background.draw();
+        for (let i = 0; i < this.topPlayers.length; ++i) {
+            this.topPlayersTexts[i].draw()
+        }
+        this.playerRateText.draw();
+    }
+}
+
+var ysdk_player_score = 0;
+function ysdk_set_score(score) {
+    YaGames.init().then(ysdk => {
+        ysdk.getLeaderboards()
+        .then(lb => {
+          lb.setLeaderboardScore('main', score);
+        });
+    });
+}
+
+
+function ysdk_get_score() {
+    YaGames.init().then(ysdk => {
+        ysdk.getLeaderboards()
+        .then(lb => {
+            ysdk.isAvailableMethod('leaderboards.getLeaderboardPlayerEntry')
+            .then(isAv => {
+                if (isAv) {
+                    lb.getLeaderboardPlayerEntry('main')
+                    .then(res => leaderboard.playerScore = res)
+                }
+            });
+        })
+    });
+}
+
+function ysdk_get_leaders() {
+    YaGames.init().then(ysdk => {
+        ysdk.getLeaderboards()
+        .then(lb => {
+            // Получение 10 топов
+            lb.getLeaderboardEntries('main', { quantityTop: 10 })
+            .then(res => res.entries);
+        });
+    });
+}
