826cf77eb99e0a9e002f6e06a063d7ccab60ae6a
Anton <56134229+Anton1320@users.noreply.github.com>
2023-01-13 15:40:20 +0300

    Update human.js

--- human.js
+++ human.js
@@ -3,7 +3,11 @@ var humanMaxScore = 0;
 function ysdk_set_human_score(score) {
     YaGames.init().then(ysdk => {
         ysdk.getPlayer().then(_player => {
-            _player.setStats({"maxScore": score});
+            _player.setStats({"maxScore": score})
+            .then(succ => {
+                if (succ) console.log("setting score succed")
+                else console.log("setting score failed")
+            });
         })
     })
 }
@@ -11,7 +15,7 @@ function ysdk_set_human_score(score) {
 function ysdk_get_human_score(score) {
     YaGames.init().then(ysdk => {
         ysdk.getPlayer().then(_player => {
-            _player.getStats(["maxScore"]).then(ans => humanMaxScore = ans.maxScore);
+            _player.getStats(["maxScore"]).then(ans => {humanMaxScore = ans.maxScore; console.log("getting score succed" + humanMaxScore)})
         })
     })
 }
